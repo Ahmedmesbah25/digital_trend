57d8ff3c9acd2dfe9d71ae8ccd47bf625d98853e
Ahmedmesbah25 <ahmedamam@ymail.com>
2021-11-11 21:24:14 +0200

    trend

---
+++ java.js
@@ -0,0 +1,18 @@
+let btn = document.querySelector(".btn")
+
+
+window.onscroll = function () {
+    if (window.scrollY >= 600) {
+        btn.style.display = "block"
+    } else {
+        btn.style.display = "none"
+    }
+}
+
+btn.onclick = function () {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+    })
+}
